9befab27dc424a58e18977a8b58fc3ec4d8eb2a7
kbberker <kbbuk90@gmail.com>
2019-05-02 15:36:45 +0100

    Render astronauts from state

--- src/AstronautContainer.js
+++ src/AstronautContainer.js
@@ -1,17 +1,35 @@
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react";
+import astronaut from "./astronaut.png";
 
 class AstronautContainer extends Component {
-  state = { peopleInSpace: null }
+  state = { peopleInSpace: null };
 
   componentDidMount() {
     fetch("http://api.open-notify.org/astros.json")
       .then(r => r.json())
-      .then(data => this.setState({ peopleInSpace: data.people }))
+      .then(data => this.setState({ peopleInSpace: data.people }));
   }
 
+  renderAstronauts = () => {
+    // For each astronaut in state.peopleInSpace
+    // render an img of astronaut
+    return this.state.peopleInSpace.map(person => {
+      return <img className="astronauts" src={astronaut} alt="astronaut" />;
+    });
+  };
+
   render() {
-    return <h1>Astronauts Here</h1>
+    return (
+      <Fragment>
+        <h1>Astronauts Here</h1>
+        {this.state.peopleInSpace ? (
+          this.renderAstronauts()
+        ) : (
+          <h1>NO ONE HERE</h1>
+        )}
+      </Fragment>
+    );
   }
 }
 
-export default AstronautContainer
+export default AstronautContainer;
